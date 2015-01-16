33daac8abe6913e16d0aca82b1f20d716a4425e1
ArthurTerminus <ArthurTerminus@gmail.com>
2015-01-16 18:42:13 +0900

    Bodies

--- Bodies/Bodies.py
+++ Bodies/Bodies.py
@@ -4,8 +4,7 @@
 import sys
 import jp.ac.kyoto_su.cse.drt.Example as example
 
+
 if __name__ == '__main__':
-		print "テスト"
-		if example != None:
-			print "成功"
-   		sys.exit(example.main())
+    an_example = example.Example()
+    sys.exit(an_example.main())
