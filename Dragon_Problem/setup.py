bb39c4d225baea76289b318da96394cd6119e8ff
ArthurTerminus <ArthurTerminus@gmail.com>
2015-01-16 16:18:52 +0900

    change setup.py

--- Dragon_Problem/setup.py
+++ Dragon_Problem/setup.py
@@ -7,11 +7,11 @@ setup(
     name = "Bodies",
     version="1.1.0"
     description="Bodies written by Python and PyOpenGL",
-    url = '',
-    author = '',
+    url = 'http://www.cse.kyoto-su.ac.jp/~drt',
+    author = 'Ishihara, Isobe, Ueda, Yoshio',
     author_email = '',
     license = '',
-    long_description =  '',
+    long_description = 'ドラゴン・スズメバチ・うさぎ・ペンギン・鬼・赤ちゃんを3次元描画する。',
     platforms = '',
-    packages=[],
+    packages=['jp'],
     )
--- project/jp/ac/kyoto-su/cse/pl/Parts/__init__.py
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""パッケージに必要な「__initi__.py」ファイル。"""
