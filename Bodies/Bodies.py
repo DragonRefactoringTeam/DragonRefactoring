007fb71789dea6210a78291f83786a349409f6ef
g1244073 <isobe@suzumu-no-MacBook-Air.local>
2015-01-16 17:26:20 +0900

    isobe

---
+++ Bodies/Bodies.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import jp.ac.kyoto_su.cse.drt.Example as example
+
+if __name__ == '__main__':
+		print "テスト"
+		if example != None:
+			print "成功"
+   		sys.exit(example.main_test())
---
+++ Bodies/jp/ac/kyoto_su/cse/drt/Example.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from jp.ac.kyoto_su.cse.drt.Bodies.Dragon import DragonModel
+from jp.ac.kyoto_su.cse.drt.Bodies.Wasp import WaspModel
+from jp.ac.kyoto_su.cse.drt.Bodies.Bunny import BunnyModel
+from jp.ac.kyoto_su.cse.drt.Bodies.Penguin import PenguinModel
+from jp.ac.kyoto_su.cse.drt.Bodies.Oni import OniModel
+from jp.ac.kyoto_su.cse.drt.Bodies.Baby import BabyModel
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+
+class Example(object):
+    """例題プログラム：ドラゴンを実行する。"""
+    def main_test(self):
+        # """ドラゴンを実行するメインプログラム。"""
+        # a_model = DragonModel()
+        # a_model.open()
+    
+        # a_model = WaspModel()
+        # a_model.open()
+    
+        # a_model = BunnyModel()
+        # a_model.open()
+    
+        # a_model = PenguinModel()
+        # a_model.open()
+    
+        # a_model = OniModel()
+        # a_model.open()
+    
+        # a_model = BabyModel()
+        # a_model.open()
+    
+        # glutMainLoop()
+
+        return 0
